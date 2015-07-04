0b6f56e3887da1df840027a46d747a83ef708686
jm2az <johnmaclaughlin@gmail.com>
2015-07-04 15:36:38 -0400

    Connected to AWS stock database

--- js/stockTable/controllers/stockTableCtrl.js
+++ js/stockTable/controllers/stockTableCtrl.js
@@ -6,8 +6,9 @@ require(["angular", "stockTable"], function(angular, stockTable) {
 	angular.module('stockTable')
 	
 	.controller('stockTableCtrl', ['$scope', '$http', function($scope, $http) {
-		$http.get('js/stockTable/stocks.json').success(function(data) {
-			$scope.stocks = data;
+		$http.get('http://ec2-52-5-86-82.compute-1.amazonaws.com:5000/stocks').success(function(data) {
+			$scope.stocks = data._items;
+			console.log("Loaded stocks");
 		});
  
  	}]);
