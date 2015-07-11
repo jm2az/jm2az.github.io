453c9be0a02816e29082fe264dfd03a8be299c47
jm2az <johnmaclaughlin@gmail.com>
2015-07-11 16:54:15 -0400

    Reformatted extra info

--- js/stockTable/controllers/stockTableCtrl.js
+++ js/stockTable/controllers/stockTableCtrl.js
@@ -10,30 +10,44 @@ require(["angular", "stockTable"], function(angular, stockTable) {
         	{ticker: "Ticker"},
         	{name: "Company Name"},
         	{roi: "Return on Investment"},
-        	{irr: "Internal Rate of Return"},
+        	{irr: "Internal Rate of Return"}
+        	/*,
         	{cur_price: "Current Price"},
         	{target: "1 Year Expected Price"},
         	{div_cash: "Annual Dividends"},
         	{beta: "Beta"},
         	{coe: "Cost of Equity"},
         	{npv: "Net Present Value"}
+        	*/
     	];
 	
+		// Load data
 		$http.get('http://ec2-52-5-86-82.compute-1.amazonaws.com:5000/stocks').success(function(data) {
 			$scope.body = data._items;
 			console.log("Loaded stocks");
+			console.log($scope.body);
 		});
 		
+		// Sorting column
 		$scope.sort = {
         	column: 'roi',
         	descending: true
     	};
+    	
+    	// Close all extra info
+    	$scope.closeInfo = function() {
+    	    $scope.body.forEach(function(element) {
+        		element.show_info = false;
+        	});
+    	};
  
+ 		// Add class for sorted column
  		$scope.selectedCls = function(column) {
  			console.log(column == $scope.sort.column && 'sort-' + $scope.sort.descending);
         	return column == $scope.sort.column && 'sort-' + $scope.sort.descending;
     	};
     
+    	// Change order for stock table
     	$scope.changeSorting = function(column) {
         	var sort = $scope.sort;
         	if (sort.column == column) {
@@ -46,11 +60,33 @@ require(["angular", "stockTable"], function(angular, stockTable) {
             		sort.descending = false;
             	}
         	}
+        	$scope.closeInfo();
     	};
     	
+    	// Green for positive numbers, red for negative ones
     	$scope.textColor = function(number) {
     		if (parseFloat(number) < 0) {return "negative";}
     		if (parseFloat(number) > 0) {return "positive";}
+    	}
+    	
+    	// Toggles if a stock's info will be shown
+    	$scope.toggleRow = function(index) {
+    		$scope.body[index].show_info = !$scope.body[index].show_info;
+    	}
+    	
+    	// Determines whether to show the stock's extra info or not
+    	$scope.showInfo = function(index) {
+    		return $scope.body[index].show_info;
+    	}
+    	
+    	// Formats dollar appropriately
+    	$scope.formatDollar = function(dollar) {
+    		return '$' + parseFloat(String(dollar).replace('-', '')).toFixed(2);
+    	}
+    	
+    	// Formats percentage appropriately
+    	$scope.formatPercentage = function(percent) {
+    		return (parseFloat(String(percent).replace("-", ""))*100).toFixed(2) + '%';
     	}
  	}]);
 });
